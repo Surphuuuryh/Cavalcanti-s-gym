0d7e54a6d55034f13e934d5148c82b32497448ca
Surphuuryh <lucasfsvaz@gmail.com>
2026-06-23 12:28:28 -0300

    primeiro exercicio cumprido

---
+++ Exercises/1exerc.ts
@@ -0,0 +1,4 @@
+let nome: string = "Surphuuuryh";
+let idade: number = 18;
+let estudandoprog: boolean = false;
+console.log("Meu nome é:", nome, "eu tenho", idade, "anos e meu status de estar estudando prog é:", estudandoprog);
