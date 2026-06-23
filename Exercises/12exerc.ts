6b435acdea1ddc5f83db6bb0ee105d7b4204df15
Surphuuryh <lucasfsvaz@gmail.com>
2026-06-23 18:11:29 -0300

    12 feito.

---
+++ Exercises/12exerc.ts
@@ -0,0 +1,7 @@
+const precos: number[] = [10, 25, 3, 40, 7];
+const precosdobrados: number[] = precos.map(n => n * 2);
+console.log("Os preços dobrados são:", precosdobrados);
+const precosfiltrados: number [] = precos.filter(n => n > 15);
+console.log("Os preços acima de 15 são:", precosfiltrados);
+const precossomados: number = precos.reduce((acumulador, numero) => acumulador + numero, 0);
+console.log("Os preços somados são:", precossomados);
