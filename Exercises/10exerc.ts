3b5fdcf97e5e1909ed9f333327cfb140b3d831cf
Surphuuryh <lucasfsvaz@gmail.com>
2026-06-23 17:53:48 -0300

    10, 11 feitos.

---
+++ Exercises/10exerc.ts
@@ -0,0 +1,10 @@
+type Predicado<T> = (item: T) => boolean;
+const isapar: Predicado<number> = (a: number): boolean => {
+    return a % 2 === 0;
+}
+
+function myfunc (valor: number[]): number[] {
+    return valor.filter(isapar);
+}
+let piranhagem: number[] = [1, 2, 3, 4];
+console.log(myfunc(piranhagem));
---
+++ Exercises/11exerc.ts
@@ -0,0 +1,2 @@
+const arraystringsprog: string[] = ["C", "C++", "C#", "HolyC", "Objective-C"];
+console.log(arraystringsprog[0], arraystringsprog[arraystringsprog.length - 1]);
