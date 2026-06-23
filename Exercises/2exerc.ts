e11e4f45ce0113ec2c4e0ac7f7779e7e471113ff
Surphuuryh <lucasfsvaz@gmail.com>
2026-06-23 12:55:02 -0300

    segundo e terceiro exercicios cumpridos.

---
+++ Exercises/2exerc.ts
@@ -0,0 +1,4 @@
+let texto = "textin";
+let numero = 67;
+let numeros = [6, 7];
+console.log(typeof texto, typeof numero, typeof numeros);
---
+++ Exercises/3exerc.ts
@@ -0,0 +1,5 @@
+const rnyear: number = 2026;
+let tries: number = 0;
+
+// rnyear = 10;
+// dá erro porque o typescript não permite mudar o valor de uma constante por ela ser uma constante.
